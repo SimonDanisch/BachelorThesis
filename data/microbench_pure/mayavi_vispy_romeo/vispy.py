3aeccfa19fd18b8020ac3be81b69b9f38ec835e4
Simon Danisch <sdanisch@gmail.com>
2015-05-05 19:21:24 +0200

    update

--- data/microbench_pure/mayavi_vispy_romeo/vispy.py
+++ data/microbench_pure/mayavi_vispy_romeo/vispy.py
@@ -19,21 +19,38 @@ canvas = scene.SceneCanvas(keys='interactive')
 view = canvas.central_widget.add_view()
 view.camera = scene.TurntableCamera(up='z')
 
+
+
 # Simple surface plot example
 # x, y values are not specified, so assumed to be 0:50
-z = gaussian_filter(np.random.normal(size=(50, 50)), (1, 1)) * 10
+z = np.fromfunction(lambda i, j: np.sin(i*j), (500, 500), dtype=float)
+
+print(type(z))
 p1 = scene.visuals.SurfacePlot(z=z, color=(0.5, 0.5, 1, 1), shading='smooth')
 p1.transform = scene.transforms.AffineTransform()
-p1.transform.scale([1/49., 1/49., 0.02])
+p1.transform.scale([1/49., 1/49., 1.0])
 p1.transform.translate([-0.5, -0.5, 0])
 
 view.add(p1)
 
 # Add a 3D axis to keep us oriented
 axis = scene.visuals.XYZAxis(parent=view.scene)
+i = 0.0
+def update(ev):
+    global p1
+    global i 
+    i += 1.0
+    p1.set_data(z=np.fromfunction(lambda x, y: np.sin((x/10.0)*(i/50.0))*np.sin((y/50.0)*(i/(x/100.0))), (500, 500), dtype=float))
+
+timer = app.Timer()
+timer.connect(update)
+timer.start(0)
+
+
 
 if __name__ == '__main__':
     canvas.show()
+    p1.set_data(z = gaussian_filter(np.random.normal(size=(100, 100)), (1, 1)) * 10)
     canvas.measure_fps()
     if sys.flags.interactive == 0:
         app.run()
