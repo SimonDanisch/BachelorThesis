01009292b59f32592a9dbefe2f10915ac523ca44
Simon Danisch <sdanisch@gmail.com>
2015-05-06 22:10:58 +0200

    update

--- data/microbench_pure/mayavi_vispy_romeo/julia.jl
+++ data/microbench_pure/mayavi_vispy_romeo/julia.jl
@@ -1,6 +1,16 @@
 using GLVisualize, AbstractGPUArray, GLAbstraction, GeometryTypes, Reactive, Meshes
 
-const robj = visualize(rand(Float32,1000,1000), :surface, model=Input(translationmatrix(Vec3(2,0,0))))
+
+function xy(x,y,i)
+	x = ((x/N)-0.5f0)*i
+	y = ((y/N)-0.5f0)*i
+	r = sqrt(x*x + y*y)
+	Float32(sin(r)/r)
+end
+create(i, N) = [xy(x,y,i) for x=1:N, y=1:N]
+const N 	= 1000
+surface 	= lift(create, bounce(1f0:80f0), N)
+const robj 	= visualize(surface.value, :surface, color_norm = Vec2(-0.5,1))
 
 push!(GLVisualize.ROOT_SCREEN.renderlist, robj)
 
