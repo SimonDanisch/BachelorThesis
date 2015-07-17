1955cbabe26e0d3e6d25c2d3aead896a930ab45a
Simon Danisch <sdanisch@gmail.com>
2015-07-17 16:45:08 +0200

    update

--- data/benchmarks/mayavi_vispy_romeo_matlab/surface.jl
+++ data/benchmarks/mayavi_vispy_romeo_matlab/surface.jl
@@ -8,9 +8,9 @@ function f(x, y, i, N)
 end
 create(i, N) = Float32[f(Float32(x),Float32(y),i,Float32(N)) for x=1:N, y=1:N]
 
-N 		= 600
-surface = lift(create, bounce(1f0:200f0), N)
-#surface = create(20f0, N)
+N 		= 900
+#surface = lift(create, bounce(1f0:200f0), N)
+surface = create(20f0, N)
 view(visualize(surface, :surface, color_norm = Vec2(-0.5,1)))
 renderloop()
 
